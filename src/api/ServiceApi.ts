e81dfa7f83ea2a49a5973b10cffd26d6c15ef674
ManishaFistreet <manisha@fistreet.in>
2025-07-17 19:30:39 +0530

    fix

--- src/api/ServiceApi.ts
+++ src/api/ServiceApi.ts
@@ -4,7 +4,7 @@ import type { PriceFormValues, Booking, BookingPayload, Coupon, ServiceShowcase,
 import type { OrderBookingFormValues } from "../types/services"
 import type { LoginLog } from "../types/services";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "http://backend:5000/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
